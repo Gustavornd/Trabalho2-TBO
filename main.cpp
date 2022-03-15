7495f18ba757d280d09cd683d8527bdd05c498c1
Gustavo Rafael Nunes Durães <gustavo.ipatinga@gmail.com>
2022-03-15 19:33:48 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+// Trabalho 2 TBO.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include <iostream>
+#include "ListaEstatica.h"
+#include "Functions.h"
+#include "menu.h"
+
+#include <locale>
+
+using namespace std;
+
+int main()
+{
+    //CreateInputFile();
+    setlocale(LC_ALL, "Portuguese");
+    Menu();
+
+
+
+    return 0;
+}
+
+
